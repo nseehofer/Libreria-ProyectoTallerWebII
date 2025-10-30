65fbab6875623518222ef177603275132cafbbc4
Diego Molver <molverdiego@gmail.com>
2025-10-30 16:30:36 -0300

    agrego rutas para productos y carrito + paleta de colores y fuente

--- app-libreria/src/app/app.routes.ts
+++ app-libreria/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ListaLibros } from './modules/libros/pages/lista-libros/lista-libros';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'libros', 
+        component: ListaLibros
+    },
+    {
+        path: 'carrito',
+        loadComponent: () => import('./modules/libros/pages/carrito/carrito').then(m => m.Carrito)
+    },
+    {
+        path: '', redirectTo: 'libros', pathMatch: 'full'
+    }
+
+];
