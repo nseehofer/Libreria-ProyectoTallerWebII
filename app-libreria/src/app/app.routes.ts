c36cb7b5733aafce6b62e617fb5605817dd6c29a
Rocio <nemethrocio@gmail.com>
2025-10-31 14:39:16 -0300

    Se agrega el registro de usuario

--- app-libreria/src/app/app.routes.ts
+++ app-libreria/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ListaLibros } from './modules/libros/pages/lista-libros/lista-libros';
 import { Carrito } from './modules/libros/pages/carrito/carrito';
 import { DetalleLibro } from './modules/libros/pages/detalle-libro/detalle-libro';
+import { RegistroUsuario } from './modules/registro-usuario/registro-usuario'; 
+
 
 export const routes: Routes = [
     {
@@ -15,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'carrito',
         component: Carrito
+    },
+    {
+    path: 'registro-usuario',
+    component: RegistroUsuario, 
     },
     {
         path: '', redirectTo: 'libros', pathMatch: 'full'
