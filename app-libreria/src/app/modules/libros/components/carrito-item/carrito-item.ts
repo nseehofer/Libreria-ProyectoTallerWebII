b9217628d3cea7042561e5ba9f66e71d4aeb2353
Diego Molver <molverdiego@gmail.com>
2025-11-01 02:00:55 -0300

    Implemento urls dinamicas para las portadas + urls seguras para solucionar los errores de la consola

--- app-libreria/src/app/modules/libros/components/carrito-item/carrito-item.ts
+++ app-libreria/src/app/modules/libros/components/carrito-item/carrito-item.ts
@@ -3,6 +3,8 @@ import {  } from '../../../../service/libros/libro.service';
 import { CarritoService } from '../../../../service/carrito/carrito.service';
 import { CurrencyPipe } from '@angular/common';
 import { ItemCarrito } from '../../../../service/carrito/carrito.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carrito-item',
@@ -10,12 +12,22 @@ import { ItemCarrito } from '../../../../service/carrito/carrito.service';
   templateUrl: './carrito-item.html',
   styleUrl: './carrito-item.css',
 })
-export class CarritoItem {
+export class CarritoItem implements OnInit {
   @Input ({required:true}) item!: ItemCarrito;
 
   private carritoService = inject(CarritoService);
+  private sanitizer: DomSanitizer = inject(DomSanitizer);
+
 
   eliminarDelCarrito(codigo: string): void {
     this.carritoService.eliminarDelCarrito(codigo);
+  }
+
+  safeImgUrl!: SafeResourceUrl;
+
+  ngOnInit(): void {
+    if (this.item.libro && this.item.libro.img_src) {
+      this.safeImgUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.item.libro.img_src);
+    }
   }
 }
--- app-libreria/src/app/modules/libros/pages/detalle-libro/detalle-libro.ts
+++ app-libreria/src/app/modules/libros/pages/detalle-libro/detalle-libro.ts
@@ -2,6 +2,7 @@ import { Component, Input, inject, OnInit } from '@angular/core';
 import { Libro, LibroService } from '../../../../service/libros/libro.service';
 import { ActivatedRoute } from '@angular/router';
 import { CarritoService } from '../../../../service/carrito/carrito.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detalle-libro',
@@ -15,6 +16,8 @@ export class DetalleLibro implements OnInit {
   private route = inject(ActivatedRoute);
   private libroService = inject(LibroService);
   private carritoService = inject(CarritoService);
+  private sanitizer: DomSanitizer = inject(DomSanitizer);
+  safeImgUrl!: SafeResourceUrl;
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -22,12 +25,17 @@ export class DetalleLibro implements OnInit {
       this.libroService.getLibroPorId(+id).subscribe({
         next: (libro) => {
           this.libro = libro;
+          if (this.libro && this.libro.img_src) {
+            this.safeImgUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.libro.img_src);
+          }
         },
         error: (error) => {
           console.error('Error al obtener el libro:', error);
         }
       });
     }
+
+    
   }
 
   agregarAlCarrito(libro: Libro): void {
