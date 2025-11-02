53f4afa36043a05a6c75bfa96d0eba92a83aabee
nseehofer <nicolasseehofer.99@hotmail.com>
2025-11-01 23:41:05 -0300

    Le agrego estilo para centrar y hacer responsive el inicio de sesion, tambien hago modificaciones para que el '<router-outlet>' se utilice desde el 'app.html' y no desde el 'navbar.html'

--- app-libreria/src/app/app.ts
+++ app-libreria/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { Footer } from './modules/shared/footer/footer';
 import { Navbar } from './modules/shared/navbar/navbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- app-libreria/src/app/modules/shared/navbar/navbar.ts
+++ app-libreria/src/app/modules/shared/navbar/navbar.ts
@@ -1,16 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { CarritoService } from '../../../service/carrito/carrito.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
   private carritoService = inject(CarritoService);
-  
+
   public contadorCarrito = this.carritoService.cantidadLibros;
 
 }
