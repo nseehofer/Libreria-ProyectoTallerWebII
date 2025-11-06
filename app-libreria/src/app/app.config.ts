578b38f94708aad598fef51c21867b29a776adb6
Gabriela <leverattomariag@gmail.com>
2025-11-05 23:49:09 -0300

    [200~feat: agregar notificación con ngx-toastr al agregar productos

--- app-libreria/src/app/app.config.ts
+++ app-libreria/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 
@@ -9,6 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient() // Importo el HttpCliente para las peticiones HTTP del back
+    provideHttpClient(), // Importo el HttpCliente para las peticiones HTTP del back
+    provideAnimations(),
+    provideToastr()
   ]
 };
